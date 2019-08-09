86f43f39e4cf9def6d0f2780cc0302c6a2a626d0
Dino Horvat <dxh3401@rit.edu>
2019-08-09 15:45:20 +0200

    Init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'swagger-editor'`, () => {
+  it(`should have as title 'angular-swagger-editor'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('swagger-editor');
+    expect(app.title).toEqual('angular-swagger-editor');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to swagger-editor!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-swagger-editor!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+declare const SwaggerEditorBundle: any;
+declare const SwaggerEditorStandalonePreset: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'swagger-editor';
+export class AppComponent implements OnInit {
+  title = 'angular-swagger-editor';
+
+  ngOnInit(): void {
+    const editor = SwaggerEditorBundle({
+      dom_id: '#swagger-editor',
+      layout: 'StandaloneLayout',
+      presets: [
+        SwaggerEditorStandalonePreset
+      ],
+      url: 'http://rackerlabs.github.io/wadl2swagger/openstack/swagger/dbaas.json'
+    });
+  }
+
 }
